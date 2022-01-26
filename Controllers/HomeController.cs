1e30c4159964ac837ef1226029d6eec14e2a1b71
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-01-26 23:04:15 +0330

    [#1] Configure the HTTP pipeline by adding middlewares and prepare for MVC (with a smaple home page)

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Local_C.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    // app.UseExceptionHandler("/Error");
+    app.UseHsts();  // Prevent redirection attacks
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
