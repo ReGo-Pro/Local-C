fa2f643b9fc182dbb9fd4773a149acdef0f112f2
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-01-30 13:16:01 +0330

    [#4] Create a downloads page, dto, read from config, put on dto, display

--- Controllers/DownloadsController.cs
+++ Controllers/DownloadsController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Local_C.Models;
 
 namespace Local_C.Controllers {
     public class DownloadsController : Controller {
+        private readonly IConfiguration _config;
+
+        public DownloadsController(IConfiguration config) {
+            _config = config;   
+        }
+
+
         public IActionResult Index() {
-            return View();
+            var viewModel = new DownloadDirectoryModel() {
+                RootDir = _config.GetSection("RootDir").Value
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ Models/DownloadDirectoryModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Local_C.Models {
+    public class DownloadDirectoryModel {
+        public string RootDir { get; set; }
+        public IEnumerable<string> SubDirectories { get; set; }
+        public IEnumerable<string> Files { get; set; }
+    }
+}
