6b1892d1c7677cdbc2f27740ce8ed842a750d704
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-01-30 12:58:27 +0330

    [#4] Create downloads controller and action link in home page for it

---
+++ Controllers/DownloadsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Local_C.Controllers {
+    public class DownloadsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
