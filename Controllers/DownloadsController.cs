f84948d1ae34e8ca21caf37a07d944c6dc35a554
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-01-30 15:00:49 +0330

    [#4] Serve PDF files form the root directory

--- Controllers/DownloadsController.cs
+++ Controllers/DownloadsController.cs
@@ -23,5 +23,19 @@ namespace Local_C.Controllers {
 
             return View(viewModel);
         }
+
+        public async Task<IActionResult> Serve(string dir, string file) {
+            if (!string.IsNullOrEmpty(file)) {
+                var fileFullName = Path.Combine(_config.GetSection("RootDir").Value, file);
+                if (!System.IO.File.Exists(fileFullName)) {
+                    return NotFound();
+                }
+
+                var theFile = await System.IO.File.ReadAllBytesAsync(Path.Combine(_config.GetSection("RootDir").Value, file));
+                return File(theFile, "application/pdf");
+            }
+
+            return NotFound();
+        }
     }
 }
